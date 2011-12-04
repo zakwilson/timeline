fc3aa35bbc7df28d1eeac33926881f6ae891f826
Zak Wilson <zak.wilson@gmail.com>
2011-12-04 01:08:04 -0500

    Added routes for JS CSS and widget

--- src/timeline/views/main.clj
+++ src/timeline/views/main.clj
@@ -1,15 +1,41 @@
 (ns timeline.views.main
   (:use noir.core
-        [hiccup core page-helpers]))
+        [hiccup core page-helpers]
+        [ring.middleware file]
+        [ring.util response]))
 
-(defpartial layout [& content]
+(defpartial layout [content & {:keys [js css]}]
   (html5
    [:head
     [:title "Timeline"]
+    (if (string? js)
+      (include-js js)
+      (map include-js js))
+    (if (string? css)
+      (include-css css)
+      (map include-css css))
     ]
    [:body
-    [:h1 "Nothing to see yet"]]
+    content
+    ]
    ))
 
-(defpage "/" []
-  (layout [:h2 "Coming soon"]))
+(defpage home "/" []
+  (layout [:div#maincontent
+           [:div#placement]]
+
+          :css ["/widget/css/aristo/jquery-ui-1.8.5.custom.css"
+                "/widget/js/timeglider/Timeglider.css"]
+          :js ["/widget/js/jquery-1.4.4.min.js"
+               "/widget/js/jquery-ui-1.8.9.custom.min.js"
+               "/widget/js/timeglider.min.js"
+               "/javascript/timeline.js"]))
+
+(post-route "/javascript/*" [*]
+            (or (file-response (str "static/javascript/" *)) :next))
+
+(post-route "/css/*" [*]
+            (or (file-response (str "static/css/" *)) :next))
+
+(post-route "/widget/*" [*]
+            (or (file-response (str "static/widget/" *)) :next))
---
+++ static/javascript/timeline.js
@@ -0,0 +1,9 @@
+$(document).ready(function () { 
+
+    var tg1 = $("#placement").timeline({
+	"min_zoom":15, 
+	"max_zoom":70, 
+	"data_source":"/widget/json_tests/js_history.json"
+    });
+    
+});
